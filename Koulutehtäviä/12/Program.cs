a63aff6c4140bc6e42ee0ba74761b14a3798ec25
pinjasie <pinja.sievanen@gmail.com>
2023-05-16 15:06:50 +0300

    Assignment no 12 finished

---
+++ Koulutehtäviä/12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Aliohjelma
+{
+	static void Main()
+	{
+		Console.WriteLine("Tehtävä 12" + "\n" + "---------------------" + "\n");
+		Console.Write("Anna luku, jonka neliön haluat tietää: "); //kysytään käyttäjältä luku laskutoimitukseen
+		int luku = int.Parse(Console.ReadLine()); //käyttäjän syöte
+		int nelio = laskeNelio(luku); //aliohjelman alustus
+		Console.WriteLine("\n" + "Luvun " + luku + " neliö on " + nelio); //näytölle tulostuu käyttäjän antama luku, sekä luvusta saatu neliö
+	}
+	static int laskeNelio(int luku) //aliohjelma
+	{
+		int nelio = luku * luku; //neliön laskutoimitus tehdään aliohjelmassa
+		return nelio; //palauttaa neliön arvon lausekkeeseen, jossa sitä kutsutaan
+	}
+}
