b946c0c0ca01529d9fe5c1e2fedf5eec87fdd0b1
pinjasie <pinja.sievanen@gmail.com>
2023-05-22 12:09:36 +0300

    Update Program.cs

--- Koulutehtäviä/7.3/Program.cs
+++ Koulutehtäviä/7.3/Program.cs
@@ -7,11 +7,11 @@ class Program
 		Console.Write("Anna lukupyramidin kerrosten määrä: ");
 		int riveja = int.Parse(Console.ReadLine()); //käyttäjän syöte, jonka perusteella kerrosten määrä määräytyy
 		int i, j; //muuttujien alustus
-		for (i = 1; i <= riveja; i++) //for-loop, jonka mukaan pyramidi tulostuu
+		for (i = 1; i <= riveja; i++) //for-loop, joka tulostaa kerrokset
 		{
-			for (j = 1; j <= i; j++)
+			for (j = 1; j <= i; j++) //sisempi for-loop, joka tulostaa rivit
 			{
-				Console.Write(j);
+				Console.Write(j); //tulostetaan pyramidi sisemmän loopin avulla
 			}
 			Console.Write("\n\r");
 		}
