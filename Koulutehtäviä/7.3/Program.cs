30a3f559e5ec6894c4ace2b800e0b883f788eaed
pinjasie <pinja.sievanen@gmail.com>
2023-05-22 12:08:20 +0300

    Assignment no 7.3 done

---
+++ Koulutehtäviä/7.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Tehtävä 7.3" + "\n" + "-----------------------" + "\n");
+		Console.Write("Anna lukupyramidin kerrosten määrä: ");
+		int riveja = int.Parse(Console.ReadLine()); //käyttäjän syöte, jonka perusteella kerrosten määrä määräytyy
+		int i, j; //muuttujien alustus
+		for (i = 1; i <= riveja; i++) //for-loop, jonka mukaan pyramidi tulostuu
+		{
+			for (j = 1; j <= i; j++)
+			{
+				Console.Write(j);
+			}
+			Console.Write("\n\r");
+		}
+	}
+}
