0d18a042df714313f309243bb1d800f16bd79e7b
pinjasie <pinja.sievanen@gmail.com>
2023-05-26 11:19:35 +0300

    Assignment no 20 done

---
+++ Koulutehtäviä/20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.Write("Anna jokin merkki: ");
+		string syote = Console.ReadLine();
+		Console.WriteLine("\n" + syote);
+		Console.WriteLine();
+
+		if (string.IsNullOrWhiteSpace(syote)) //jos käyttäjä syöttää tyhjän merkin, tämä lause tulostuu
+		{
+			Console.WriteLine("Annoit tyhjän merkin.");
+		}
+		else
+		{
+			char merkki = syote[0]; //tutkitaan syötteen ensimmäinen merkki ehtolauseen avulla
+
+			if (char.IsDigit(merkki)) //jos merkki on numer
+			{
+				Console.WriteLine(merkki + " on numero.");
+			}
+			else if (char.IsUpper(merkki)) //jos merkki on iso kirjain
+			{
+				Console.WriteLine(merkki + " on iso kirjain.");
+			}
+			else if (char.IsLower(merkki)) //jos merkki on pieni kirjain
+			{
+				Console.WriteLine(merkki + " on pieni kirjain.");
+			}
+			else
+			{
+				Console.WriteLine(merkki + " on erikoismerkki."); //jos merkki on jokin erikoismerkki
+			}
+		}
+	}
+}
